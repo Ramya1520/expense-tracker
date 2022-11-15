b2fe636d5ae899220bbb2a68af587a65ce73d382
Ramya1520 <113229257+Ramya1520@users.noreply.github.com>
2022-11-15 07:50:48 +0530

    expensehandletracker

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function handleTransaction(){
+   var totalAmount = document.getElementById("demo");
+   var enteredAmount = document.getElementById("inputbox").value;
+    // document.getElementById("demo").innerHTML = parseInt(totalAmount.innerHTML) + parseInt(enteredAmount)
+    // document.getElementById("demo").innerHTML = parseInt(totalAmount.innerHTML) - parseInt(enteredAmount);
+    // if(onclick==true){
+    //     document.getElementById("demo").innerHTML = parseInt(totalAmount.innerHTML) + parseInt(enteredAmount);
+    // }
+    // else{
+    //     document.getElementById("demo").innerHTML = parseInt(totalAmount.innerHTML) - parseInt(enteredAmount);
+    // }
+    onclick ? expressionIfTrue :expressionIfFalse
+
+}
+   
+
+
