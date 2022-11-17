b565c61b288331792ec0c6f6c4f346ed2f6e32e4
Ramya1520 <113229257+Ramya1520@users.noreply.github.com>
2022-11-17 17:50:56 +0530

    expense tracker

--- index1.js
+++ index1.js
@@ -1,4 +1,8 @@
 var arr=[];
+var a=[];
+var operation;
+var d = []
+
 function transaction(input) {
    var x= document.getElementById("demo").innerHTML;
    var y= document.getElementById("num").value;
@@ -6,8 +10,10 @@ function transaction(input) {
    const d =new Date();
    var add= parseInt(x) + parseInt(y);
    var sub= parseInt(x) - parseInt(y);
-   let operation = (input==true)? "credit":"debit";
-   let operations = (input==true)? add:sub;  
+   document.getElementById('last_click').value=input;
+
+   let operation = (input=='credit')? "credit":"debit";
+   let operations = (input=='credits')? add:sub;  
     let transctiondetails={};
     transctiondetails.amount=document.getElementById("num").value;
    
@@ -17,23 +23,30 @@ function transaction(input) {
     transctiondetails.date=d;
     console.log(transctiondetails);
     arr.push(transctiondetails);
-    console.log(arr);
+
+
     }
 
 function gnrt(){
-    var table = document.getElementById("myTable");
-    for(i=0;i<arr.length;i++){
-        console.log(arr[i],i);
-        //let operation=(input==true)?add:sub;
+    document.getElementById("myTable").innerHTML = "";
 
-        var row = table.insertRow();
+    var table = document.getElementById("myTable");
+    var type =  document.getElementById("last_click").value;
+    for(i=0;i<arr.length;i++){
+        // console.log(arr[i],i);
+        // console.log("operation")
+        //let operation=(input==true)?add:sub;
+        
+
+        var row = table.insertRow();	
 
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
+        var cell5 = row.insertCell(4);                                                   
       
+
        cell1.innerHTML = arr[i].type;
        cell2.innerHTML = arr[i].amount;
        cell3.innerHTML= arr[i].balance;
@@ -41,6 +54,21 @@ function gnrt(){
        cell5.innerHTML = arr[i].date;
 
        table.appendChild(row)
+       if(arr[i].type=='credit'){
+           // console.log(a)
+           d.push(arr[i])
+           
+           console.log(d[0])
+           console.log("---------------------------")
+
+           a.push('transctiondetails')
+           console.log(a)
+       }
+       else if((type=='debit')){
+           console.log("hai");
+       }
      }
-    
+
+     console.log(d,"d")
     }
+
